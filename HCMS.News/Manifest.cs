bc91b15ce9452798292fdf6203592a02ce45c548
Matt <matt.kenney@Gmail.com>
2018-11-08 08:33:53 -0600

    Remove HCMS.News Folder

--- HCMS.News/Manifest.cs
+++
@@ -1,9 +0,0 @@
-using OrchardCore.Modules.Manifest;
-
-[assembly: Module(
-    Name = "Forms Module",
-    Author = "Matt Kenney",   
-    Version = "0.0.1",
-    Description = "HCMS Connection",   
-    Category = "Training"
-)]
